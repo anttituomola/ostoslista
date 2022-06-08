2b669f581d42f4a794a700b6a3fb2aee5bcba3c6
Antti <anttituomola8@gmail.com>
2022-06-08 10:48:27 +0300

    Check for input existing

--- components/createRecipe/AddRecipeRow.js
+++ components/createRecipe/AddRecipeRow.js
@@ -5,7 +5,8 @@ import { useRouter } from "next/router"
 
 const AddRecipeRow = () => {
     const router = useRouter()
-    const [amountPerPerson, setAmountPerPerson] = useState(0)
+    const [addedIngredients, setAddedIngredients] = useState([])
+    const [amountPerPerson, setAmountPerPerson] = useState()
     const [ingredientInput, setIngredient] = useState("")
     const [measureUnit, setMeasureUnit] = useState("")
     const measureUnits = ["g", "kg", "ml", "l", "kpl", "puntti",]
@@ -17,78 +18,89 @@ const AddRecipeRow = () => {
     }
 
     const saveIngredients = () => {
-        console.log("clicked")
-        const ingredient = {
-            id: uuid(),
-            name: ingredientInput,
-        }
-        const recipeRow = {
-            id: uuid(),
-            amountPerPerson: parseFloat(amountPerPerson),
-            name: ingredientInput,
-            unit: measureUnit,
-            recipeId: router.query.recipeId,
-            ingredientId: ingredient.id,
-        }
-
-
-        fetch("/api/addIngredient", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(ingredient),
-        })
-            .then((res) => {
-                console.log(res)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-            .then(() => {
-                fetch("/api/addRecipeRow", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(recipeRow),
-                })
-                    .then((res) => {
-                        console.log(res)
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                    }
-                    )
+        if (ingredientInput && measureUnit && amountPerPerson) {
+            const ingredient = {
+                id: uuid(),
+                name: ingredientInput,
             }
-            )
+            const recipeRow = {
+                id: uuid(),
+                amountPerPerson: parseFloat(amountPerPerson),
+                name: ingredientInput,
+                unit: measureUnit,
+                recipeId: router.query.recipeId,
+                ingredientId: ingredient.id,
+            }
+
+
+            fetch("/api/addIngredient", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(ingredient),
+            })
+                .then((res) => {
+                    console.log(res)
+                    setAddedIngredients([...addedIngredients, ingredient.name])
+                    setIngredient("")
+                    setMeasureUnit("")
+                    setAmountPerPerson("")
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+
+                .then(() => {
+                    fetch("/api/addRecipeRow", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(recipeRow),
+                    })
+                        .then((res) => {
+                            console.log(res)
+                        })
+                        .catch((err) => {
+                            console.log(err)
+                        }
+                        )
+                }
+                )
+        }
     }
 
-
-        return (
-            <>
-                <h1>Add ingredients for {router.query.recipeName}</h1>
-                <div>
-                    <span>Ingredient</span>
-                    <input type="text" onChange={(event) => setIngredient(event.target.value)} />
-                </div>
-                <div>
-                    <span>Amount per person</span>
-                    <input type="number" onChange={(event) => setAmountPerPerson(event.target.value)} />
-                </div>
-                <div>
-                    <span>Unit</span>
-                    {measureUnits.map(unit => <button
-                        onClick={() => unitSelector(unit)}
-                        key={unit}
-                        className={measureUnit === unit ? styles.selected : styles.seasonButton}>
-                        {unit}
-                    </button>)}
-                </div>
-                <button onClick={() => saveIngredients()}>Save ingredient</button>
-            </>
-        )
+    const goToAddNewRecipe = () => {
+        router.push("/admin/createRecipe")
     }
 
-    export default AddRecipeRow
+    return (
+        <>
+            <h1>Add ingredients for {router.query.recipeName}</h1>
+            <div>
+                <span>Ingredient</span>
+                <input type="text" onChange={(event) => setIngredient(event.target.value)} value={ingredientInput} />
+            </div>
+            <div>
+                <span>Amount per person</span>
+                <input type="number" onChange={(event) => setAmountPerPerson(event.target.value)} value={amountPerPerson} />
+            </div>
+            <div>
+                <span>Unit</span>
+                {measureUnits.map(unit => <button
+                    onClick={() => unitSelector(unit)}
+                    key={unit}
+                    className={measureUnit === unit ? styles.selected : styles.seasonButton}>
+                    {unit}
+                </button>)}
+            </div>
+            <button onClick={() => saveIngredients()}>Save ingredient</button>
+            <h3>Added ingredients:</h3>
+            {addedIngredients.map(ingredient => <p key={ingredient}>{ingredient}</p>)}
+            <button onClick={() => goToAddNewRecipe()}>Add new recipe</button>
+        </>
+    )
+}
+
+export default AddRecipeRow
